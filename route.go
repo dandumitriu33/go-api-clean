207777d01da6640f31d6e79ae1c1995d3d539dec
Dan Dumitriu <dandumitriu33@gmail.com>
2022-08-01 09:35:23 +0300

    Add mySQL DB repo

--- route.go
+++ route.go
@@ -2,39 +2,44 @@ package main
 
 import (
 	"encoding/json"
+	"go-api-clean/entity"
+	"go-api-clean/repository"
 	"log"
 	"net/http"
 )
 
-type Post struct {
-	Id    int    `json:"id"`
-	Title string `json:"title"`
-	Text  string `json:"text"`
-}
-
 var (
-	posts []Post
+	// posts []entity.Post
+	repo repository.PostRepository = repository.NewPostRepository()
 )
 
-func init() {
-	posts = []Post{{Id: 1, Title: "Title One", Text: "Text One"}}
-}
+// func init() {
+// 	posts = []entity.Post{{ID: 1, Title: "Title One", Text: "Text One"}}
+// }
 
 func getPosts(resp http.ResponseWriter, req *http.Request) {
-	resp.Header().Set("Content-type", "application/json")
-	result, err := json.Marshal(posts)
+	posts, err := repo.FindAll()
 	if err != nil {
 		log.Println(err)
 		resp.WriteHeader(http.StatusInternalServerError)
-		resp.Write([]byte(`{"error": "Error marshalling the posts array"}`))
+		resp.Write([]byte(`{"error": "Error getting data."}`))
 		return
 	}
+	resp.Header().Set("Content-type", "application/json")
 	resp.WriteHeader(http.StatusOK)
-	resp.Write(result)
+	json.NewEncoder(resp).Encode(posts)
+	// result, err := json.Marshal(posts)
+	// if err != nil {
+	// 	log.Println(err)
+	// 	resp.WriteHeader(http.StatusInternalServerError)
+	// 	resp.Write([]byte(`{"error": "Error marshalling the posts array"}`))
+	// 	return
+	// }
+	//resp.Write(result)
 }
 
 func addPost(resp http.ResponseWriter, req *http.Request) {
-	var post Post
+	var post entity.Post
 	err := json.NewDecoder(req.Body).Decode(&post)
 	if err != nil {
 		log.Println(err)
@@ -42,16 +47,20 @@ func addPost(resp http.ResponseWriter, req *http.Request) {
 		resp.Write([]byte(`{"error": "Error unmarshalling the request"}`))
 		return
 	}
-	post.Id = len(posts) + 1
-	posts = append(posts, post)
+	// post.ID = len(posts) + 1
+	post.ID = 0
+	// posts = append(posts, post)
+	repo.Save(&post)
+
 	resp.Header().Set("Content-type", "application/json")
 	resp.WriteHeader(http.StatusOK)
-	result, err := json.Marshal(post)
-	if err != nil {
-		log.Println(err)
-		resp.WriteHeader(http.StatusInternalServerError)
-		resp.Write([]byte(`{"error": "Error marshalling the post"}`))
-		return
-	}
-	resp.Write(result)
+	json.NewEncoder(resp).Encode(post)
+	// result, err := json.Marshal(post)
+	// if err != nil {
+	// 	log.Println(err)
+	// 	resp.WriteHeader(http.StatusInternalServerError)
+	// 	resp.Write([]byte(`{"error": "Error marshalling the post"}`))
+	// 	return
+	// }
+	// resp.Write(result)
 }
